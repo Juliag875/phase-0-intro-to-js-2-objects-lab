840d6371e9d451932a5ff3f1156318018d2c0aaa
Iuliia El Sammak <juliag875@gmail.com>
2022-01-12 21:17:45 -0500

    commit tree fee027f1fdb7feaa24271a3f0d3366d669853b73

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const employee = {
-    name: "Joe",
+    name : "Sam",
     streetAddress: "11 Broadway",
 };
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-   const newEmployee = {...employee};
-   newEmployee[key]=value;
-   return newEmployee;
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
-}
+} 
 
 function deleteFromEmployeeByKey(employee, key) {
     const newEmployee = {...employee};
@@ -24,4 +24,3 @@ function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
 }
-
